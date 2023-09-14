4c8be30c44628980add5add69e094d9e315d0fb3
BUFFVODAKA <peppy16001@gmail.com>
2023-09-14 14:58:15 +0700

    del

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -5,26 +5,42 @@ import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
   readDB();
+  
+  // const foundroom = DB.rooms.find((x)=> x.roomid ==  roomid)
+  // if(!foundroom){
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  return NextResponse.json(
+    {
+      ok: false,
+      message: `Room is not found`,
+    },
+    { status: 404 }
+  );
+  
 };
+// 
+ 
+// return NextResponse.json(
+//   {
+//     ok: true,
+//     roomid: DB.roomid,
+//     messageId: DB.messageId,
+//     messageText: DB.messageText
+
+//   }
+// )
+
 
 export const POST = async (request) => {
   readDB();
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  return NextResponse.json(
+    {
+      ok: false,
+      message: `Room is not found`,
+    },
+    { status: 404 }
+  );
 
   const messageId = nanoid();
 
@@ -39,14 +55,15 @@ export const POST = async (request) => {
 
 export const DELETE = async (request) => {
   const payload = checkToken();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  if(!payload){
+  return NextResponse.json(
+    {
+      ok: false,
+      message: "Invalid token",
+    },
+    { status: 401 }
+  );
+  }
 
   readDB();
 
